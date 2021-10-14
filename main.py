7d983389e3688ca914bcf9e4dd0892675e053d50
mustakshaikh <mushtaqtheone@gmail.com>
2021-10-14 18:07:20 -0500

    Checkin initial version of code

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+lsta=[1,2,3]
+lst2da=[[1,2,3,4],[5,6,7,8]]
+a=np.array(lsta)
+b=np.array(lst2da)
+
+print(np.random.randint(3))
